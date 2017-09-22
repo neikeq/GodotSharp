c1b589f2a7a0fcf2be23b12c38cc42c0a1691fef
Ignacio Etcheverry <ignalfonsore@gmail.com>
2017-09-22 19:12:11 +0200

    Update Rect2.cs

--- mono/glue/cs_files/Rect2.cs
+++ mono/glue/cs_files/Rect2.cs
@@ -6,20 +6,29 @@ namespace Godot
     [StructLayout(LayoutKind.Sequential)]
     public struct Rect2 : IEquatable<Rect2>
     {
-        public Vector2 pos;
-        public Vector2 size;
+        private Vector2 position;
+        private Vector2 size;
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public Vector2 Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
 
         public Vector2 End
         {
-            get { return pos + size; }
+            get { return position + size; }
         }
 
         public float Area
         {
-            get
-            {
-                return size.width * size.height;
-            }
+            get { return get_area(); }
         }
 
         public Rect2 clip(Rect2 b)
@@ -27,112 +36,149 @@ namespace Godot
             Rect2 newRect = b;
 
             if (!intersects(newRect))
-            {
-                newRect.pos.x = Mathf.max(b.pos.x, pos.x);
-                newRect.pos.y = Mathf.max(b.pos.y, pos.y);
+                return new Rect2();
 
-                Vector2 bEnd = b.End;
-                Vector2 end = End;
+            newRect.position.x = Mathf.max(b.position.x, position.x);
+            newRect.position.y = Mathf.max(b.position.y, position.y);
 
-                newRect.size.width = Mathf.min(bEnd.x, end.x) - newRect.pos.x;
-                newRect.size.height = Mathf.min(bEnd.y, end.y) - newRect.pos.y;
+            Vector2 bEnd = b.position + b.size;
+            Vector2 end = position + size;
 
-                return newRect;
-            }
+            newRect.size.x = Mathf.min(bEnd.x, end.x) - newRect.position.x;
+            newRect.size.y = Mathf.min(bEnd.y, end.y) - newRect.position.y;
 
-            return new Rect2();
+            return newRect;
         }
 
         public bool encloses(Rect2 b)
         {
-            return (b.pos.x >= pos.x) &&
-                    (b.pos.y >= pos.y) &&
-                    ((b.pos.x + b.size.width) < (pos.x + size.width)) &&
-                    ((b.pos.y + b.size.height) < (pos.y + size.height));
+            return (b.position.x >= position.x) && (b.position.y >= position.y) &&
+               ((b.position.x + b.size.x) < (position.x + size.x)) &&
+               ((b.position.y + b.size.y) < (position.y + size.y));
         }
 
-        public Rect2 expand(Vector2 b)
+        public Rect2 expand(Vector2 to)
         {
             Rect2 expanded = this;
 
-            Vector2 begin = expanded.pos;
-            Vector2 end = expanded.End;
+            Vector2 begin = expanded.position;
+            Vector2 end = expanded.position + expanded.size;
 
-            if (b.x < begin.x)
-                begin.x = b.x;
-            if (b.y < begin.y)
-                begin.y = b.y;
+            if (to.x < begin.x)
+                begin.x = to.x;
+            if (to.y < begin.y)
+                begin.y = to.y;
 
-            if (b.x > end.x)
-                end.x = b.x;
-            if (b.y > end.y)
-                end.y = b.y;
+            if (to.x > end.x)
+                end.x = to.x;
+            if (to.y > end.y)
+                end.y = to.y;
 
-            expanded.pos = begin;
+            expanded.position = begin;
             expanded.size = end - begin;
 
             return expanded;
+        }
+
+        public float get_area()
+        {
+            return size.x * size.y;
         }
 
         public Rect2 grow(float by)
         {
             Rect2 g = this;
 
-            g.pos.x -= by;
-            g.pos.y -= by;
-            g.size.width += by * 2;
-            g.size.height += by * 2;
+            g.position.x -= by;
+            g.position.y -= by;
+            g.size.x += by * 2;
+            g.size.y += by * 2;
+
+            return g;
+        }
+
+        public Rect2 grow_individual(float left, float top, float right, float bottom)
+        {
+            Rect2 g = this;
+
+            g.position.x -= left;
+            g.position.y -= top;
+            g.size.x += left + right;
+            g.size.y += top + bottom;
+
+            return g;
+        }
+
+        public Rect2 grow_margin(int margin, float by)
+        {
+            Rect2 g = this;
+
+            g.grow_individual((GD.MARGIN_LEFT == margin) ? by : 0,
+                    (GD.MARGIN_TOP == margin) ? by : 0,
+                    (GD.MARGIN_RIGHT == margin) ? by : 0,
+                    (GD.MARGIN_BOTTOM == margin) ? by : 0);
 
             return g;
         }
 
         public bool has_no_area()
         {
-            return size.width <= 0 || size.height <= 0;
+            return size.x <= 0 || size.y <= 0;
         }
 
         public bool has_point(Vector2 point)
         {
-            return point.x >= pos.x &&
-                    point.y >= pos.y &&
-                    point.x < (pos.x + size.width) &&
-                    point.y < (pos.y + size.height);
+            if (point.x < position.x)
+                return false;
+            if (point.y < position.y)
+                return false;
+
+            if (point.x >= (position.x + size.x))
+                return false;
+            if (point.y >= (position.y + size.y))
+                return false;
+
+            return true;
         }
 
         public bool intersects(Rect2 b)
         {
-            return pos.x < (b.pos.x + b.size.width) &&
-                    b.pos.x < (pos.x + size.width) &&
-                    pos.y < (b.pos.y + b.size.height) &&
-                    b.pos.y < (pos.y + size.height);
+            if (position.x > (b.position.x + b.size.x))
+                return false;
+            if ((position.x + size.x) < b.position.x)
+                return false;
+            if (position.y > (b.position.y + b.size.y))
+                return false;
+            if ((position.y + size.y) < b.position.y)
+                return false;
+
+            return true;
         }
 
         public Rect2 merge(Rect2 b)
         {
             Rect2 newRect;
 
-            newRect.pos.x = Mathf.min(b.pos.x, pos.x);
-            newRect.pos.y = Mathf.min(b.pos.y, pos.y);
+            newRect.position.x = Mathf.min(b.position.x, position.x);
+            newRect.position.y = Mathf.min(b.position.y, position.y);
 
+            newRect.size.x = Mathf.max(b.position.x + b.size.x, position.x + size.x);
+            newRect.size.y = Mathf.max(b.position.y + b.size.y, position.y + size.y);
 
-            newRect.size.x = Mathf.max(b.pos.x + b.size.width, pos.x + size.width);
-            newRect.size.y = Mathf.max(b.pos.y + b.size.height, pos.y + size.height);
-
-            // Make relative again
-            newRect.size = newRect.size - newRect.pos;
+            newRect.size = newRect.size - newRect.position; // Make relative again
 
             return newRect;
         }
 
-        public Rect2(Vector2 pos, Vector2 size)
+        public Rect2(Vector2 position, Vector2 size)
         {
-            this.pos = pos;
+            this.position = position;
             this.size = size;
         }
 
         public Rect2(float x, float y, float width, float height)
         {
-            this.pos = new Vector2(x, y);
+            this.position = new Vector2(x, y);
             this.size = new Vector2(width, height);
         }
 
@@ -158,19 +204,19 @@ namespace Godot
 
         public bool Equals(Rect2 other)
         {
-            return pos.Equals(other.pos) && size.Equals(other.size);
+            return position.Equals(other.position) && size.Equals(other.size);
         }
 
         public override int GetHashCode()
         {
-            return pos.GetHashCode() ^ size.GetHashCode();
+            return position.GetHashCode() ^ size.GetHashCode();
         }
 
         public override string ToString()
         {
             return String.Format("({0}, {1})", new object[]
             {
-                this.pos.ToString(),
+                this.position.ToString(),
                 this.size.ToString()
             });
         }
@@ -179,7 +225,7 @@ namespace Godot
         {
             return String.Format("({0}, {1})", new object[]
             {
-                this.pos.ToString(format),
+                this.position.ToString(format),
                 this.size.ToString(format)
             });
         }
